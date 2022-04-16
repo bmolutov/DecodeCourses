7831af0bd91a74d2829846827618b02e55f1af8b
Bekzat Molutov <b_molutov@kbtu.kz>
2022-04-16 16:27:26 +0600

    new

--- Python/1course/thirdMonth/webParsing/2s/2s.py
+++ Python/1course/thirdMonth/webParsing/2s/2s.py
@@ -23,26 +23,26 @@ headers = {
 # with open("index.html", "w", encoding="utf-8") as file:
 #     file.write(src)
 
-# with open("index.html", encoding="utf-8") as file:
-#     src = file.read()
+with open("index.html", encoding="utf-8") as file:
+    src = file.read()
 
 ##########################################################
 # creating json file and writing categories
 
-# soup = BeautifulSoup(src, "lxml")
-# all_products_hrefs = soup.find_all(class_="mzr-tc-group-item-href")
-# base_url = "https://health-diet.ru"
+soup = BeautifulSoup(src, "lxml")
+all_products_hrefs = soup.find_all(class_="mzr-tc-group-item-href")
+base_url = "https://health-diet.ru"
 
-# all_categories_dict = {}
-# for item in all_products_hrefs:
-#     item_text = item.text
-#     item_href = base_url + item.get("href")
-#     # print(f"{item_text}: {item_href}")
-#     all_categories_dict[item_text] = item_href
+all_categories_dict = {}
+for item in all_products_hrefs:
+    item_text = item.text
+    item_href = base_url + item.get("href")
+    # print(f"{item_text}: {item_href}")
+    all_categories_dict[item_text] = item_href
 
 # saving data into json file
-# with open("all_categories_dict.json", "w", encoding="utf-8") as file:
-#     json.dump(all_categories_dict, file, indent=4, ensure_ascii=False)
+with open("all_categories_dict.json", "w", encoding="utf-8") as file:
+    json.dump(all_categories_dict, file, indent=4, ensure_ascii=False)
 
 ############################################################
 
