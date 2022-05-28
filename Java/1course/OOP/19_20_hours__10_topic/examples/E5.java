24f8e7a0cdd1383c1151af0debf9673ef4d0c2b8
Bekzat Molutov <b_molutov@kbtu.kz>
2022-05-28 18:59:58 +0600

    upd

---
+++ Java/1course/OOP/19_20_hours__10_topic/examples/E5.java
@@ -0,0 +1,12 @@
+public class E5 {
+	public static void main(String[] args) {
+		checkAge(12);
+	}
+	static void checkAge(int age) {
+		if(age < 18) {
+			throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+		} else {
+			System.out.println("Access granted - You are old enough!");
+		}
+	}
+}
